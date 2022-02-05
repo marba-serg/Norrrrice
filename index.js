b23fffc3952c561db790928bc213eaf100fe7f64
marba-serg <marba@ukr.net>
2022-02-05 19:18:36 +0200

    add: add change foto and color

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-let joke = document.querySelector('.joke');
+const joke = document.querySelector('.joke');
 let jokeBtn = document.querySelector('.joke-btn');
 let img = document.querySelector('.img');
 let wrap = document.querySelector('.wrap');
-
-let color = ['red', 'pink', 'blue']
+const foto = document.querySelector('.foto');
+let color = ['PeachPuff ', 'PaleTurquoise ', 'LightSalmon ', 'Khaki ', 'LightSteelBlue ', 'LightPink']
+let i = 0
 
 
 jokeBtn.addEventListener('click', generateJoke)
@@ -12,21 +13,23 @@ generateJoke()
 
 // USING ASYNC/AWAIT
 async function generateJoke() {
-  
+
   const config = {
     headers: {
       Accept: 'application/json',
     },
   }
 
-const res = await fetch('https://api.chucknorris.io/jokes/random', config)
+  const res = await fetch('https://api.chucknorris.io/jokes/random', config)
 
   const data = await res.json()
 
+
+  wrap.style.backgroundColor = color[i]
+  if (i > 5) i = 0
+  else {
+    foto.src = `assets/img/${i}.png`
+    i++
+  }
   joke.innerText = data.value;
-  img.src = data.icon_url
- 
-
-
-  
 }
